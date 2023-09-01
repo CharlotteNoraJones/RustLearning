848de73d45b3e88f958ef1978a944fb30e9d5dcb
CharlotteNoraJones <47127622+CharlotteNoraJones@users.noreply.github.com>
2023-09-01 18:43:00 -0500

    Hello World Rust

---
+++ hello_cargo/target/debug/deps/hello_cargo-e1f562bedf636e7b.d
@@ -0,0 +1,5 @@
+D:\Documents\GitHub\Rust\hello_cargo\target\debug\deps\hello_cargo-e1f562bedf636e7b.rmeta: src\main.rs
+
+D:\Documents\GitHub\Rust\hello_cargo\target\debug\deps\hello_cargo-e1f562bedf636e7b.d: src\main.rs
+
+src\main.rs:
